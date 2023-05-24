62a9b18ffff78f86e8a3ea82437654c42baf544b
meryemipekci <meryemdelgurgen@hotmail.com>
2023-05-24 15:52:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieList = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrows, i) => {
+    const widhtRatio = Math.floor(window.innerwidth / 300);
+    let clickCounter = 0;
+    const imageItem = movieList[i].querySelectorAll("img").length;
+
+    arrows.addEventListener("click", function () {
+        clickCounter++;
+     if (imageItem - (4 + clickCounter) >= 0) {
+          
+            movieList[i].style.transform = `translateX(${
+                movieList[i].computedStyleMap().get("transform")[0].x.value-300
+            }px)`;
+        }else{
+            movieList[i].style.transform = "translateX(0)";
+            clickCounter = 0;
+        }
+    });
+    });
+
+    /* dark mode*/
+
+    const ball = document.querySelector(".toggle-ball");
+    const items = document.querySelectorAll(
+    ".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-list-title"
+    );
+
+   
+
+    ball.addEventListener("click", function () {
+    items.forEach((item) => item.classList.toggle("active"));
+
+    });
+   
